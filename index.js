ab8f26b2b42ec7fe3cf4f940e82ac79a5b4b0732
Borja Lorente <blorente@ucm.es>
2017-07-29 14:48:24 +0200

    Add dirty rest endpoints

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:dbpass@localhost:5433/ld';
+
+const client = new pg.Client(connectionString);
+client.connect();
+const query = client.query(
+  'CREATE TABLE IF NOT EXISTS messages(id SERIAL PRIMARY KEY, content TEXT not null, upvotes INT)');
+query.on('end', () => { client.end(); });
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -10,7 +20,27 @@ app.get('/', function (req, res) {
 })
 
 app.get('/messages', function (req, res) {
-  res.send('Get Messages from DB')
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM messages ORDER BY id ASC;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
 })
 
 app.get('/bookstatus', function (req, res) {
@@ -19,10 +49,62 @@ app.get('/bookstatus', function (req, res) {
 
 app.post('/message', function (req, res) {
   console.log(req.body);
+  const results = [];
+  // Grab data from http request
+  const data = {msg: req.body.message, upvotes: 0};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Insert Data
+    client.query('INSERT INTO messages(content, upvotes) values($1, $2)',
+    [data.msg, data.upvotes]);
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM messages ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
 })
 
 app.post('/upvote', function (req, res) {
-  console.log(req.body);
+  const results = [];
+  // Grab data from the URL parameters
+  const id = parseInt(req.body.mid);
+  console.log('Upvoting msg ' + id);
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Update Data
+    client.query('UPDATE messages SET content=content, upvotes=upvotes + 1 WHERE id=($1)',
+    [id]);
+    // SQL Query > Select Data
+    const query = client.query("SELECT * FROM messages ORDER BY id ASC");
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
 })
 
 app.listen(3000, function () {
